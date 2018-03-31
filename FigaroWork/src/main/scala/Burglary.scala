1614e2a698a53bfcd0e9824ab80b1fa353ecf099
Arijit Pramanik <parijit10@gmail.com>
2018-03-31 20:52:19 +0800

    Add fiagro working directory

---
+++ FigaroWork/src/main/scala/Burglary.scala
@@ -0,0 +1,24 @@
+import com.cra.figaro.algorithm.factored._
+import com.cra.figaro.language._
+import com.cra.figaro.library.compound._
+object Burglary {
+Universe.createNew()
+private val burglary = Flip(0.01)
+private val earthquake = Flip(0.0001)
+private val alarm = CPD(burglary, earthquake,
+(false, false) -> Flip(0.001),
+(false, true) -> Flip(0.1),
+(true, false) -> Flip(0.9),
+(true, true) -> Flip(0.99))
+private val johnCalls = CPD(alarm,
+false -> Flip(0.01),
+true -> Flip(0.7))
+def main(args: Array[String]) {
+johnCalls.observe(true)
+val alg = VariableElimination(burglary, earthquake)
+alg.start()
+println("Probability of burglary: " + alg.probability(burglary,
+true))
+alg.kill
+}
+}
---
+++ FigaroWork/src/main/scala/test1.scala
@@ -0,0 +1,6 @@
+import com.cra.figaro.language._
+object test1 {
+	def main(args: 	Array[String]) {
+		val HelloWorldElement = Constant("Hello World!")
+	}
+}
